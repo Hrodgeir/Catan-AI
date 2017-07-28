2e53c4470329095ba66ba666b4bdf455e9e8cf48
Jose Gordillo <gordillo@uvic.ca>
2017-07-28 14:55:04 -0700

    now we have turns and get resources based on dice roll

--- project.py
+++ project.py
@@ -1,21 +1,25 @@
 from board import *
 from display import *
 from player import *
+from game_engine import *
 import vertex
 
 def main():
     try:
         board = Board()
-        players = {}
+        engine = GameEngine()
+        players = []
         i = 1
 
         # create players
         while (i <= 4):
-            players[i] = Player(i, None)
-            i =  i + 1
+            players.append(Player(i, None))
+            i = i + 1
     
         # the vertices that we have to iterate over and make calculations to choose the best
         avail_vertices = board.get_available_vertices()
+
+        engine.positioning_turns(players)
 
         # Initialize the display
         display = Display(board)
