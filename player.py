19c8a7e5f8ecf1638ce759511ed0fd455ddc44d6
Matthew James Hodgson <matthodg@uvic.ca>
2017-08-13 13:31:18 -0700

    Added the decision made and more buttons

--- player.py
+++ player.py
@@ -13,6 +13,7 @@ class Player:
         self.vertex_distances = []
         self.points = 0
         self.knights = 0
+        self.decision = ""
 
     def set_strategy(self, new_strategy):
         self.new_strategy = new_strategy
